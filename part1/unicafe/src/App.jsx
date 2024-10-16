f1da94da5fcff538e19bb965e6c432e92aba8e88
Glauber Nascimento <glauber.oliveira91@gmail.com>
2024-10-16 20:11:24 -0300

    Unicafe Update
    Done 1.7, 1.8, 1.9 and 1.10 exercises for Unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,11 +1,27 @@
 import { useState } from 'react'
 
-
-const Statistic = ({ text, value }) => (
+const StatisticLine = (props) => (
   <div>
-    {text} {value}
+    {props.text} {props.value}
   </div>
 )
+
+const Statistics = (props) => {
+  if(props.good + props.neutral + props.bad == 0) {
+    return <p>No feedback given</p>
+  } else {
+    return (
+      <div>
+        <StatisticLine text='good' value={props.good}/>
+        <StatisticLine text='neutral' value={props.neutral}/>
+        <StatisticLine text='bad' value={props.bad}/>
+        <StatisticLine text='all' value={props.good+props.neutral+props.bad}/>
+        <StatisticLine text='average' value={(props.good-props.bad)/(props.good+props.neutral+props.bad)}/>
+        <StatisticLine text='positive' value={((props.good)/(props.good+props.neutral+props.bad)*100) + ' %'}/>
+      </div>
+    )
+  }
+}
 
 const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>
@@ -31,6 +47,7 @@ const App = () => {
     setBad(bad + 1)
   }
 
+
   return(
     <div>
       <h1>give feedback</h1>
@@ -38,9 +55,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
       <h1>statistics</h1>
-      <Statistic text='good' value={good}/>
-      <Statistic text='neutral' value={neutral}/>
-      <Statistic text='bad' value={bad}/>
+      <Statistics good={good} bad={bad} neutral={neutral}/>
     </div>
   )
 }
