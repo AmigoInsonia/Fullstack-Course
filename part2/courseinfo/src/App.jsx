4bf16e8cdca5e7af21595c7000bbf7c8b2275911
Glauber Nascimento <glauber.oliveira91@gmail.com>
2024-10-22 14:20:19 -0300

    All exercises from 2a done
    Finished exercise 2.5, so now all exercises from 2a are done. Moving on to 2b now!

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,44 +1,4 @@
-const Header = (props) => {
-  console.log("Header: ", props.course)
-  return <h1>{props.course}</h1>
-}
-
-const Total = (props) => {
-  return <strong>Number of exercises: {props.sumOfExercises}</strong>
-}
-
-const Part = (props) => {
-  console.log("Part data: ", props)
-  return (
-    <p>
-      {props.name} {props.exercises}
-    </p>
-  )
-}
-
-const Content = ({content}) => {
-  console.log("Content data: ", content)
-  return (
-    <div>
-      {content.map(content =>
-        <Part name={content.name} exercises={content.exercises} key={content.id}/>
-      )}
-    </div>
-  )
-}
-
-const Course = (course) => {
-  console.log(course)
-  return(
-    <div>
-      <Header course={course.name}/>
-      <Content content={course.parts}/>
-      <Total sumOfExercises={course.parts.reduce(
-        (accumulator, currentValue) => accumulator + currentValue.exercises, 0
-      )}/>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
