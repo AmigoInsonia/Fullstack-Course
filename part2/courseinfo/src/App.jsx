beaf221cf0e67a7173d92ffcb146c1c8ee924192
Glauber Nascimento <glauber.oliveira91@gmail.com>
2024-10-22 13:52:17 -0300

    Started Exercises from Part 2
    Part 2.1 exercise done!

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,66 @@
+const Header = (props) => {
+  console.log(props.course)
+  return <h1>{props.course}</h1>
+}
+
+const Total = (props) => {
+  return <p>Number of exercises {props.sumOfExercises}</p>
+}
+
+const Part = (props) => {
+  console.log("Part data: ", props)
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  )
+}
+
+const Content = ({content}) => {
+  console.log("Content data: ", content)
+  return (
+    <div>
+      {content.map(content =>
+        <Part name={content.name} exercises={content.exercises} key={content.id}/>
+      )}
+    </div>
+  )
+}
+
+const Course = ({course}) => {
+  console.log(course)
+  return(
+    <div>
+      <Header course={course.name}/>
+      <Content content={course.parts}/>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
