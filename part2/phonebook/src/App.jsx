da4451725eb15fb024a2b8ad2dbf3af62dbce3cf
Glauber Nascimento <glauber.oliveira91@gmail.com>
2024-11-05 16:40:52 -0300

    2.8 Done
    Finished 2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,14 +4,22 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas'} 
+    { name: 'Arto Hellas', number: '040-1234567'} 
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
+
+    if (newName.trim() === '') {
+      alert('Name cannot be empty!')
+      return
+    }
+
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     let addName = true
@@ -28,11 +36,17 @@ const App = () => {
       setPersons(persons.concat(nameObject))
     }
       setNewName('')
+      setNewNumber('')
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -43,6 +57,12 @@ const App = () => {
           name: <input 
             value={newName}
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
