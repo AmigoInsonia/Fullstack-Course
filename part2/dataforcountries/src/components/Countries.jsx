1af069388c11154bfb42565bbdc715d0eb166147
Glauber Nascimento <glauber.oliveira91@gmail.com>
2025-01-16 13:42:52 -0300

    Finished 2.19
    Finished exercise 2.19*: Data for countries, step 2
    I couldn't find an easy way to change the button from "show" to "hide", so I just left it as it is for now.

--- part2/dataforcountries/src/components/Countries.jsx
+++ part2/dataforcountries/src/components/Countries.jsx
@@ -3,7 +3,15 @@ import axios from 'axios'
 
 const Countries = ({ filteredCountries }) => {
 
-	
+	function showCountry(countryID) {
+		var country = document.getElementById(countryID)
+		if (country.style.display === "none") {
+			country.style.display = "block";
+		}
+		else {
+			country.style.display = "none";
+		}
+	}
 
 	if (filteredCountries.length === 0) {
 		console.log("Countries is null!")
@@ -43,7 +51,24 @@ const Countries = ({ filteredCountries }) => {
 		console.log(filteredCountries)
 		return (
 			<div>
-				{filteredCountries.map((filteredCountries) => <li key={filteredCountries.name.common}>{filteredCountries.name.common}</li>)}
+				{filteredCountries.map((filteredCountries) => 
+					<div>
+						<div key={filteredCountries.name.common}>{filteredCountries.name.common}
+							<button onClick={() => showCountry(filteredCountries.name.common)}>
+						      show
+						    </button>
+						</div>
+
+					    <div id={filteredCountries.name.common} style={{display:"none"}}>
+					            <div>capital {filteredCountries.capital[0]}</div>
+								<div>area {filteredCountries.area}</div>
+
+								<h2>languages</h2>
+								{Object.entries(filteredCountries.languages).map(([key, value]) => <li key={key}>{value}</li>)}
+								<img src={filteredCountries.flags.png} />
+					    </div>					    
+					</div>
+					)}					
 			</div>
 		)
 	}
